9e4f892368be9c39afec3d2b7daf4a0cac7ceb07
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-11-04 12:40:26 +0600

    Complete lab 9

--- Lab 9/simpson.py
+++ Lab 9/simpson.py
@@ -8,6 +8,7 @@ av = []
 av2 = []
 av3 = []
 av4 = []
+av5 = []
 trv = []
 
 
@@ -66,6 +67,23 @@ def Trapezium_Rule(h, a, b):
     return sum * h
 
 
+def Simpson_Rule(h, a, b):
+    sum = 0.0
+
+    i = a
+    while i < b:
+        x0 = i
+        x1 = i + h
+        x2 = i + 2 * h
+
+        sum += (fx(x0) + 4 * fx((x0 + x1) / 2) + fx(x1)) / 6
+        sum += (fx(x1) + 4 * fx((x1 + x2) / 2) + fx(x2)) / 6
+
+        i += 2 * h
+
+    return sum * h
+
+
 def perform_integration():
     tv = A(0, 4)
 
@@ -117,6 +135,16 @@ def perform_integration4():
 
         print(f"dx = {h:.6f} a=0 b=4\n  Value = {ans_n:.6f}\n")
 
+def perform_integration5():
+    tv = A(0, 4)
+
+    for it in range(1, 10001):
+        h = it / 10000
+
+        ans_n = Simpson_Rule(h, 0, 4)
+        av5.append(ans_n)
+
+        print(f"dx = {h:.6f} a=0 b=4\n  Value = {ans_n:.6f}\n")
 
 if __name__ == "__main__":
     print("\nLEFT REIMANN SUM:\n\n")
@@ -127,12 +155,15 @@ if __name__ == "__main__":
     perform_integration3()
     print("\nTRAPEZOID RULE:\n\n")
     perform_integration4()
+    print("\nSIMPSON'S RULE:\n\n")
+    perform_integration5()
 
     plt.plot(dx, trv, label="True Value")
     plt.plot(dx, av, label="Left Reimann Sum")
     plt.plot(dx, av2, label="Right Reimann Sum")
     plt.plot(dx, av3, label="Mid Reimann Sum")
     plt.plot(dx, av4, label="Trapezium Rule")
+    plt.plot(dx, av5, label="Trapezium Rule")
     plt.xlabel("dx")
     plt.ylabel("Approx. Value")
     plt.title("Numerical Integration")
